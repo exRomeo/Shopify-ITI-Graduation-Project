1f6a1949eb925a29e2f46d94b95bfe732884ce13
Ramy Ashraf <Ramy3r@hotmail.com>
2023-06-15 00:24:59 +0300

    Linked Cart and Wish lists to user account also handled deleting list when removing last item as draft orders should contain at least one item to exist and doesn't allow removing last item so in other words i don't remove last item i remove whole draft order and then add another draft order when adding product for first time imagine the agony i have to go through because we're using the wrong api "insert palm face imoji"

--- app/src/main/java/com/example/shopify/core/helpers/CurrentUserHelper.kt
+++ app/src/main/java/com/example/shopify/core/helpers/CurrentUserHelper.kt
@@ -3,10 +3,12 @@ package com.example.shopify.core.helpers
 import android.util.Log
 import com.example.shopify.data.repositories.authentication.IAuthRepository
 import com.example.shopify.presentation.screens.settingsscreen.TAG
-
+/*
+    should be used for testing :D
 const val USER_ID = 7121192321330
 const val WISHLIST_DRAFT_ORDER_ID = 1121329742130
 const val CART_DRAFT_ORDER = 1121338360114
+*/
 
 object CurrentUserHelper {
 
@@ -26,7 +28,10 @@ object CurrentUserHelper {
             customerID = ids.customer_id!!
             cartDraftID = ids.card_id!!
             wishlistDraftID = ids.wishlist_id!!
-            Log.i(TAG, "GOT IDZZZZ: $customerID, $cartDraftID, $wishlistDraftID")
+            Log.i(
+                TAG,
+                "Current User Data: Customer id -> $customerID,Cart Draft ID -> $cartDraftID, Wishlist Draft ID -> $wishlistDraftID"
+            )
         }
     }
 
